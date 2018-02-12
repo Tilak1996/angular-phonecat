b8160b67021c3c325be69db9a6cc8b0cbc158e7e
Tilak1996 <tilakpatel@ymail.com>
2018-02-12 20:18:35 +0530

    added filters.

--- app/app.module.js
+++ app/app.module.js
@@ -5,6 +5,17 @@ var phonecatApp = angular.module('phonecatApp',[
 ]);
 
 //Define PhoneListController in phonecatApp module
-phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
-    
-});
+// phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
+//     $scope.phones = [
+//         {
+//             name: 'Nexus S',
+//             snippet: 'Fast just got faster with Nexus S.'
+//         }, {
+//             name: 'Motorola XOOM with Wi-Fi',
+//             snippet: 'The next generation tablet.'
+//         }, {
+//             name: 'Motorola XOOM',
+//             snippet: 'The Next, Next generation tablet.'
+//         }
+//     ];
+// });
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -3,10 +3,26 @@
 // Angular E2E Testing Guide:
 // https://docs.angularjs.org/guide/e2e-testing
 
-describe('My app', function() {
+describe('phonecatApp', function() {
 
   beforeEach(function() {
     browser.get('index.html');
+  });
+
+  it('should filter as user types into the `Search` box', function() {
+    var phonelist = element.all(by.repeater('phone in $ctrl.phones'));
+    var query = element(by.model('$ctrl.query'));
+
+    expect(phonelist.count()).toBe(3);
+
+    //Test if we type nexus in it
+    query.sendKeys('nexus');
+    expect(phonelist.count()).toBe(1);
+    query.clear();
+
+    //Test if we type motorola in it
+    query.sendKeys('motorola');
+    expect(phonelist.count()).toBe(2);
   });
 
 });
